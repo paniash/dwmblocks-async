c1355083aab78eaf8804bcf0a08f09773b0cf7cb
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2023-09-05 14:14:26 +0530

    added async repo

---
+++ config.h
@@ -0,0 +1,12 @@
+#define CMDLENGTH 80
+#define DELIMITER " │"
+// #define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("smusic", 0, 11),
+	BLOCK("pipevolume", 0, 10),
+	BLOCK("brightness", 0, 19),
+	BLOCK("sbattery", 20, 9),
+	BLOCK("sdate", 0, 17),
+	BLOCK("stime", 60, 18)
+};
